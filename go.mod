4d467ef60b00acd64df1a4a398eae1f6c6e3dd9f
szymon676 <sgil73072@gmail.com>
2023-07-20 09:36:55 +0200

    verify booking req and create booking

--- go.mod
+++ go.mod
@@ -3,12 +3,23 @@ module github.com/szymon676/deskly
 go 1.20
 
 require (
+	github.com/gofiber/fiber v1.14.6
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
 )
 
 require (
+	github.com/andybalholm/brotli v1.0.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/mattn/go-colorable v0.1.7 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.16.0 // indirect
+	github.com/valyala/tcplisten v0.0.0-20161114210144-ceec8f93295a // indirect
+	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
 )
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"log"
+	"errors"
+	"time"
 
+	"github.com/gofiber/fiber"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -13,6 +15,32 @@ type Booking struct {
 	UserID    int    `gorm:"column:user_id"`
 	StartTime string `gorm:"column:start_time"`
 	EndTime   string `gorm:"column:end_time"`
+}
+
+// check if booking struct is correct, if not return false if yes return true
+func VerifyBooking(booking *Booking) bool {
+	if booking.DeskID == 0 || booking.UserID == 0 {
+		return false
+	}
+
+	startTime, err := time.Parse(time.RFC3339, booking.StartTime)
+	if err != nil {
+		// Invalid start time format
+		return false
+	}
+
+	endTime, err := time.Parse(time.RFC3339, booking.EndTime)
+	if err != nil {
+		// Invalid end time format
+		return false
+	}
+
+	if endTime.Before(startTime) {
+		// End time is before start time
+		return false
+	}
+
+	return true
 }
 
 type Storage struct {
@@ -35,8 +63,13 @@ func NewStorage(dataSourceName string) (*Storage, error) {
 	}, nil
 }
 
-func (s *Storage) CreateBooking(b *Booking) error {
-	result := s.db.Create(b)
+func (s *Storage) CreateBooking(booking *Booking) error {
+	ok := VerifyBooking(booking)
+	if !ok {
+		return errors.New("bad booking struct")
+	}
+
+	result := s.db.Create(booking)
 	if result.Error != nil {
 		return result.Error
 	}
@@ -44,10 +77,18 @@ func (s *Storage) CreateBooking(b *Booking) error {
 	return nil
 }
 
+type apiserver struct {
+	Storage
+}
+
+func (a *apiserver) handleCreateBokking(c *fiber.Ctx) error {
+	return c.JSON("placeholder")
+}
+
 func main() {
-	storage, err := NewStorage("username:password@tcp(host:port)/database_name")
+	storage, err := NewStorage("root:1234@tcp(127.0.0.1:3306)/main")
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 	_ = storage
 }
