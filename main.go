14e57e67f95de50fa63494ce668842a9bf09c7b4
szymon676 <sgil73072@gmail.com>
2023-08-09 12:46:22 +0200

    stop container when exited

--- main.go
+++ main.go
@@ -1,13 +1,39 @@
 package main
 
-// func init() {
-// 	opts := &mysql.MySQLContainerOptions{
-// 		RootPassword: "1234",
-// 		Database:     "main",
-// 	}
-// 	container := mysql.NewMySQLContainer(opts)
-// 	container.Run()
-// }
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/szymon676/betterdocker/mysql"
+)
+
+func init() {
+	opts := &mysql.MySQLContainerOptions{
+		RootPassword: "1234",
+		Database:     "main",
+	}
+	container := mysql.NewMySQLContainer(opts)
+	container.Run()
+
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go func() {
+
+		sig := <-sigs
+		fmt.Println()
+		fmt.Println(sig)
+		done <- true
+	}()
+
+	<-done
+	container.Stop()
+}
 
 func main() {
 	storage, err := NewStorage("root:1234@tcp(127.0.0.1:3306)/main")
